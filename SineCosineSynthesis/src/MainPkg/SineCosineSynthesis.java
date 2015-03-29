a9b81a124322531ef758c5439abe102a86c39bce
JeongSeokPark <black3184@nate.com>
2015-03-30 00:33:52 +0900

    Main class initialization

--- SineCosineSynthesis/src/MainPkg/SineCosineSynthesis.java
+++ SineCosineSynthesis/src/MainPkg/SineCosineSynthesis.java
@@ -1,5 +1,40 @@
 package MainPkg;
 
-public class SineCosineSynthesis {
+import java.awt.Graphics;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JApplet;
+import javax.swing.JButton;
+
+import java.awt.Color;
+
+public class SineCosineSynthesis extends JApplet implements ActionListener{
+
+	public void init(){
+		
+	}
+	
+	public void start(){
+		
+	}
+	
+	public void paint(Graphics g){
+		
+	}
+	
+	public void stop(){
+		
+	}
+	
+	public void destroy(){
+		
+	}
+	
+	@Override
+	public void actionPerformed(ActionEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
